affd5199947d74e6e831c757663b86028edcc0d3
joaol23 <joaolucas.m.ferreira2008@gmail.com>
2024-10-09 11:42:34 -0300

    chore: ajustando link do linkedin

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,3 +1,3 @@
-export const LINKEDIN_URL = 'https://www.linkedin.com/in/joao-lucas-mesquita-ferreira-334aa6205/';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/joao-lucas-mesquita-ferreira';
 export const EMAIL = 'joaolucas.m.ferreira2008@gmail.com';
 export const GITHUB_URL = 'https://github.com/joaol23';
